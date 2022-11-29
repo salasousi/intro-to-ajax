de5bebad8f9e3d26290a8cbea77fc405e24eed10
salasousi <102608394+salasousi@users.noreply.github.com>
2022-11-29 11:49:57 -0500

    Add files via upload

--- intro-to-ajax/js/script.js
+++ intro-to-ajax/js/script.js
@@ -1,13 +1,57 @@
 
-const promise = $.ajax({
-    url:'https://www.omdbapi.com/?apikey=2b9698f3&t=BladeRunner2045'
-});
+//////////////////
+// VARIABLES
+//////////////////
+const URL = 'https://www.omdbapi.com/?apikey=2b9698f3&t=';
 
-promise.then(
-  (data) => {
-   console.log(data);
-  },
-  (error) => {
-   console.log('bad request: ', error);
-  }
-);
+
+//////////////////
+// ELEMENTS REF'D / CACHED ELEMENTS
+//////////////////
+const $title = $("#title")
+const $year = $("#year")
+const $rated = $("#rated")
+const $form = $("form")
+const $input = $("input[type='text']")
+
+//////////////////
+// EVENT LISTENERS
+//////////////////
+$form.on("submit", handleGetData);
+
+
+//////////////////
+// FUNCTIONS
+//////////////////
+
+
+// promise.then(
+//   (data) => {
+//    console.log(data);
+//   },
+//   (error) => {
+//    console.log('bad request: ', error);
+//   }
+// );
+
+//Starting at the $.ajax method below is the same as the above but 
+//different syntax (as well as surrounded by the handleGetData function)
+
+function handleGetData(event){
+    event.preventDefault();
+    userInput= $input.val();
+    if (userInput === "") return;
+
+    $input.val("");
+    
+    $.ajax(URL + userInput).then((data) => {
+        console.log(data)
+        $title.text(data.Title);
+        $year.text(data.Year);
+        $rated.text(data.Rated);
+        //$("main").append(`<img src="${data.Poster}" alt="${data.Title}"/>`);
+        $("main>div").html(`<img id="movieImg" src="${data.Poster}"/>`)
+    }, (error) => {
+        console.log("bad request ", error)
+    })
+}
